17d3aa86f89b2b0a9e1db57b41ae10d62f1959fc
Yoshimatsu Kio <lilkiossgang@gmail.com>
2021-01-20 21:57:31 +0900

    Make components touchable

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// import MemoListScreen from './src/ Screens/MemoListScreen';
+import MemoListScreen from './src/ Screens/MemoListScreen';
 // import MemoDetailScreen from './src/ Screens/MemoDetailScreen';
 // import MemoEditScreen from './src/ Screens/MemoEditScreen';
 // import MemoCreateScreen from './src/ Screens/MemoCreateScreen';
 // import LogInScreen from './src/ Screens/LogInScreen';
-import SignUpScreen from './src/ Screens/SignUpScreen';
+// import SignUpScreen from './src/ Screens/SignUpScreen';
 
 export default function App() {
   return (
-    <SignUpScreen />
+    <MemoListScreen />
   );
 }
