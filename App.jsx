4e6c0bf6819b3d65fcf9dd00849b28efc8b22880
Yoshimatsu Kio <lilkiossgang@gmail.com>
2021-01-20 21:42:03 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // import MemoListScreen from './src/ Screens/MemoListScreen';
 // import MemoDetailScreen from './src/ Screens/MemoDetailScreen';
-import MemoEditScreen from './src/ Screens/MemoEditScreen';
+// import MemoEditScreen from './src/ Screens/MemoEditScreen';
 // import MemoCreateScreen from './src/ Screens/MemoCreateScreen';
+// import LogInScreen from './src/ Screens/LogInScreen';
+import SignUpScreen from './src/ Screens/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
