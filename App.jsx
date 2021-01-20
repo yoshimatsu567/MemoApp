0e6a01a5144b0891bd81d2249c6ea415cc54708b
Yoshimatsu Kio <lilkiossgang@gmail.com>
2021-01-20 20:46:58 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import MemoListScreen from './src/ Screens/MemoListScreen';
+// import MemoListScreen from './src/ Screens/MemoListScreen';
 // import MemoDetailScreen from './src/ Screens/MemoDetailScreen';
+// import MemoEditScreen from './src/ Screens/MemoEditScreen';
+import MemoCreateScreen from './src/ Screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
